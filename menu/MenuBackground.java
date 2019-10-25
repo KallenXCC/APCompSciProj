afc08e9c6f971c3541656e76ab03c789c44f6c6e
Braeton Buckley <bbuck001@ucr.edu>
2019-10-25 09:35:59 -0700

    Add files via upload

---
+++ menu/MenuBackground.java
@@ -0,0 +1,8 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class MenuBackground extends World
+{
+    public MenuBackground()
+    {    
+        super(1024, 768, 1); 
+    }
+}
